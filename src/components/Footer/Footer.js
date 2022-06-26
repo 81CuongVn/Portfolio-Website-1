d33ccf29b9e065435eba63f5ab12a6d789022490
blaze-pvt <talwadkerdinesh@gmail.com>
2022-06-26 11:53:17 +0530

    updated info on navbar and footer pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:314-343-3432">123-456-789</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:dineshtalwadker@gmail.com">
+            dineshtalwadker@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
